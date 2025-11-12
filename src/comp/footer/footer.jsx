d18d82f279d97d7b5b30f6fc482028301c006e48
hardeep-2025 <hardeep@iosandweb.net>
2025-11-12 09:52:23 +0530

    all

--- src/comp/footer/footer.jsx
+++ src/comp/footer/footer.jsx
@@ -3,6 +3,10 @@ import logo from '../header/headerImg/logo.png'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import communication from "./footerImg/communication (4) 1 (1).png"
+import social  from "./footerImg/social (11) 1 (1).png"
+import twitter from "./footerImg/twitter (18) 1 (1).png"
+import styles from 'yet-another-react-lightbox/styles.css';
 
 function Footer() {
   return (
@@ -15,16 +19,16 @@ function Footer() {
     <div className={Style.FooterFirstPar1}><img className={Style.FooterFirstParimg1}  src={logo} alt="" />
     <p className={Style.FooterFirstPartPra1}>At the heart of innovation and excellence, we bring visionary real estate developments that redefine modern living. Our upcoming project on the Patiala–Chandigarh Highway blends design, comfort, and luxury to create spaces that inspire a better lifestyle.</p>
     </div>
-    <div >
-        <h6>Useful Links</h6>
-        <div>
-            <div><ul>
+    <div className={Style.FooterFirstPart2} >
+        <h6 className={Style.FooterFirstPart2Head}>Useful Links</h6>
+        <div className={Style.FooterFirstPart2ListDiv}>
+            <div className={Style.FooterFirstPart2ListStyle}><ul>
             <li>About</li>
             <li>Key Highlights</li>
             <li>Price List</li>
             <li>Floor Plan</li>
             </ul></div>
-            <div><ul>
+            <div className={Style.FooterFirstPart2ListStyle}><ul>
             <li>About</li>
             <li>Key Highlights</li>
             <li>Price List</li>
@@ -33,9 +37,11 @@ function Footer() {
             </div>
     </div>
 
-    <div>
-        <h6>Follow Us On</h6>
-        <div><img src="" alt="" /><img src="" alt="" /><img src="" alt="" /></div>
+    <div className={Style.FooterFirstPart3}>
+        <h6>Follow Us</h6>
+        <div className={Style.FooterFirstPart3ImgCounter}><img src={communication} alt="" />
+        <img src={twitter} alt="" />
+        <img src={twitter} alt="" /></div>
     </div>
     </div>
     
